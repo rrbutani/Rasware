41b00c68993b284c460fbc8396a6093c7808dfaa
Srinjoy Majumdar <srinjoymajumdar@wireless-10-146-107-161.public.utexas.edu>
2016-10-26 21:01:23 -0500

    robot is now possessed and led changes color maybe on ir sensor

--- IRSensor/Main.c
+++ IRSensor/Main.c
@@ -1,9 +1,14 @@
 
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/adc.h>
+#include <RASLib/inc/pwm.h>
+#include <RASLib/inc/gpio.h>
+
 
 static tADC *adc[2];
 static tBoolean initialized = false;
+static tPWM *g;
+
 
 void initIRSensor(void) {
     // don't initialize this if we've already done so
@@ -15,28 +20,30 @@ void initIRSensor(void) {
 
     // initialize 4 pins to be used for ADC input
     adc[0] = InitializeADC(PIN_D0);
-    adc[1] = InitializeADC(PIN_D1);
+    // adc[1] = InitializeADC(PIN_D1);
 
 }
 
 void IRSensorDemo(void) {
-    Printf("Press any key to quit\n");
+    // Printf("Press any key to quit\n");
 
     // loop as long as the user doesn't press a key
-    while (!KeyWasPressed()) {
-        Printf(
-            "IR values:  %1.3f  %1.3f\r",
-            ADCRead(adc[0]),
-            ADCRead(adc[1])
-            );
-    }
-
-    Printf("\n");
+    // while (!KeyWasPressed()) {
+        // Printf(
+        //     "IR values:  %1.3f  %1.3f\r",
+        //     ADCRead(adc[0]),
+        //     // ADCRead(adc[1])
+        //     );
+    // }
+		SetPWM(g, ADCRead(adc[1]), 0.0f);
+    // Printf("\n");
 }
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
     //CallEvery(blink, 0, 0.5);
+		g = InitializePWM(PIN_F3, 50.0f);
+		SetPWM(g, 0.5, 0.0f);
     initIRSensor();
     while (1) {
         // Runtime code can go here
